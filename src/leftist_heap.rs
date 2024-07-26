7c75527bc00d49470ac64f859407e99fe64c90d2
Yusuke Nojima <nojima@ynojima.com>
2024-07-27 01:40:50 +0900

    Add leftist heap

---
+++ src/leftist_heap.rs
@@ -0,0 +1,282 @@
+use std::{
+    cmp::min,
+    mem::swap,
+    ptr::{null_mut, NonNull},
+};
+
+#[derive(Debug)]
+pub struct Hook {
+    left: *mut Hook,
+    right: *mut Hook,
+    parent: *mut Hook,
+    s: usize,
+}
+
+impl Default for Hook {
+    fn default() -> Self {
+        Self {
+            left: null_mut(),
+            right: null_mut(),
+            parent: null_mut(),
+            s: 0,
+        }
+    }
+}
+
+// Melds the given two heaps and returns the root of the merged heap.
+// O(log n)
+pub unsafe fn meld<Entry: Ord, const OFFSET: usize>(
+    root1: *mut Hook,
+    root2: *mut Hook,
+    parent: *mut Hook,
+) -> *mut Hook {
+    if root1.is_null() {
+        if root2.is_null() {
+            return null_mut();
+        }
+        unsafe { (*root2).parent = parent };
+        return root2;
+    }
+    if root2.is_null() {
+        unsafe { (*root1).parent = parent };
+        return root1;
+    }
+
+    // make sure that root1 <= root2
+    let (root1, root2) = unsafe {
+        // NOTE: we create shared references to Entry here.
+        let entry1 = (root1.byte_sub(OFFSET) as *mut Entry)
+            .as_ref()
+            .unwrap_unchecked();
+        let entry2 = (root2.byte_sub(OFFSET) as *mut Entry)
+            .as_ref()
+            .unwrap_unchecked();
+        if entry1 > entry2 {
+            (root2, root1)
+        } else {
+            (root1, root2)
+        }
+    };
+
+    // do meld
+    unsafe {
+        (*root1).right = meld::<Entry, OFFSET>((*root1).right, root2, root1);
+        if (*root1).left.is_null() || (*(*root1).left).s < (*(*root1).right).s {
+            swap(&mut (*root1).left, &mut (*root1).right);
+        }
+        (*root1).s = if (*root1).right.is_null() { 0 } else { (*(*root1).right).s } + 1;
+        (*root1).parent = parent;
+    }
+    root1
+}
+
+// Inserts new_node into the heap and returns new root.
+// O(log n)
+pub unsafe fn push<Entry: Ord, const OFFSET: usize>(
+    root: *mut Hook,
+    new_node: *mut Hook,
+) -> *mut Hook {
+    unsafe {
+        assert_ne!(new_node, null_mut(), "new_node must not be null");
+        assert_eq!((*new_node).left, null_mut(), "new_node.left must be null");
+        assert_eq!((*new_node).right, null_mut(), "new_node.right must be null");
+        assert_eq!(
+            (*new_node).parent,
+            null_mut(),
+            "new_node.parent must be null"
+        );
+    }
+    unsafe { meld::<Entry, OFFSET>(root, new_node, null_mut()) }
+}
+
+// Removes minimum element of the heap and returns (new_root, min_entry).
+// O(log n)
+pub unsafe fn pop_min<Entry: Ord, const OFFSET: usize>(
+    root: *mut Hook,
+) -> (*mut Hook, Option<NonNull<Entry>>) {
+    if root.is_null() {
+        return (null_mut(), None);
+    }
+    unsafe { assert_eq!((*root).parent, null_mut(), "root.parent must be null") };
+    unsafe {
+        let new_root = meld::<Entry, OFFSET>((*root).left, (*root).right, null_mut());
+        (*root).left = null_mut();
+        (*root).right = null_mut();
+        let entry = NonNull::new_unchecked(root.byte_sub(OFFSET) as *mut Entry);
+        (new_root, Some(entry))
+    }
+}
+
+// Removes the given node from the heap and returns new root.
+// O(log n)
+pub unsafe fn unlink<Entry: Ord, const OFFSET: usize>(
+    root: *mut Hook,
+    node: *mut Hook,
+) -> *mut Hook {
+    assert_ne!(root, null_mut(), "root must not be null");
+    assert_ne!(node, null_mut(), "node must not be null");
+    assert_eq!((*root).parent, null_mut(), "root.parent must be null");
+
+    let subtree = meld::<Entry, OFFSET>((*node).left, (*node).right, (*node).parent);
+
+    // node の親からのリンクを subtree へのリンクに置き換える
+    let parent = (*node).parent;
+    if !parent.is_null() {
+        if (*parent).left == node {
+            (*parent).left = subtree;
+        } else {
+            (*parent).right = subtree;
+        }
+    }
+
+    // node が削除されたことで、node の祖先のノードについて leftist property が破れている可能性がある。
+    // よって、node の祖先を調べて leftist property が再び満たされるように修正していく。
+    let mut p = parent;
+    while !p.is_null() {
+        let sl = if (*p).left.is_null() { 0 } else { (*(*p).left).s };
+        let sr = if (*p).right.is_null() { 0 } else { (*(*p).right).s };
+        // leftist property が破れていたら修正する
+        if sl < sr {
+            swap(&mut (*p).left, &mut (*p).right);
+        }
+        let new_s = min(sl, sr) + 1;
+        // 一度 s の値が変化しないノードがあれば、それ以降はずっと変化しないことが保証されるので、
+        // ループを打ち切ってよい。この打ち切りをしないと計算量が O(n) になってしまう。
+        if new_s == (*p).s {
+            break;
+        }
+        (*p).s = new_s;
+        p = (*node).parent;
+    }
+
+    (*node).left = null_mut();
+    (*node).right = null_mut();
+    (*node).parent = null_mut();
+
+    if node == root {
+        subtree
+    } else {
+        root
+    }
+}
+
+pub unsafe fn visit_all<Entry: Ord, const OFFSET: usize>(
+    root: *mut Hook,
+    f: &mut impl FnMut(NonNull<Entry>),
+) {
+    if root.is_null() {
+        return;
+    }
+    let entry = NonNull::new_unchecked(root.byte_sub(OFFSET) as *mut Entry);
+    f(entry);
+    visit_all::<Entry, OFFSET>((*root).left, f);
+    visit_all::<Entry, OFFSET>((*root).right, f);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::{cell::UnsafeCell, collections::BTreeSet, mem::offset_of};
+
+    #[derive(Debug, Default)]
+    struct Entry {
+        x: i64,
+        hook: UnsafeCell<Hook>,
+    }
+    impl PartialEq for Entry {
+        fn eq(&self, other: &Self) -> bool {
+            self.x == other.x
+        }
+    }
+    impl Eq for Entry {}
+    impl PartialOrd for Entry {
+        fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+            Some(self.cmp(other))
+        }
+    }
+    impl Ord for Entry {
+        fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+            self.x.cmp(&other.x)
+        }
+    }
+
+    const HOOK_OFFSET: usize = offset_of!(Entry, hook);
+
+    #[derive(Debug)]
+    enum Action {
+        Push(i64),
+        PopMin,
+        Unlink(usize),
+    }
+
+    fn print_heap(root: *mut Hook) {
+        let mut v = Vec::new();
+        unsafe {
+            visit_all::<Entry, HOOK_OFFSET>(root, &mut |entry| {
+                v.push(entry.as_ref().x);
+            });
+        }
+        println!("{:?}", v);
+    }
+
+    #[test]
+    fn randomized_test() {
+        let mut expected = BTreeSet::new();
+        let mut root: *mut Hook = null_mut();
+
+        for i in 0..10000 {
+            let action = {
+                if expected.is_empty() {
+                    Action::Push(i)
+                } else {
+                    match rand::random::<usize>() % 10 {
+                        0..5 => Action::Push(i),
+                        5..8 => Action::PopMin,
+                        8..10 => Action::Unlink(rand::random::<usize>() % expected.len()),
+                        _ => unreachable!(),
+                    }
+                }
+            };
+            print_heap(root);
+            println!("action = {:?}", action);
+            match action {
+                Action::Push(x) => {
+                    expected.insert(x);
+
+                    let new_entry = Box::leak(Box::new(Entry {
+                        x,
+                        ..Default::default()
+                    }));
+                    root = unsafe { push::<Entry, HOOK_OFFSET>(root, new_entry.hook.get_mut()) };
+                }
+                Action::PopMin => {
+                    let expected_min = *expected.first().unwrap();
+                    expected.remove(&expected_min);
+
+                    let (new_root, min_entry) = unsafe { pop_min::<Entry, HOOK_OFFSET>(root) };
+                    root = new_root;
+                    let actual_min = unsafe { min_entry.unwrap().as_ref().x };
+                    assert_eq!(expected_min, actual_min);
+                }
+                Action::Unlink(idx) => {
+                    let expected_x = *expected.iter().nth(idx).unwrap();
+                    expected.remove(&expected_x);
+
+                    // heap では idx 番目に小さい値が不明なので、expected_x を探して削除するということにする
+                    let mut x_entry = None;
+                    unsafe {
+                        visit_all::<Entry, HOOK_OFFSET>(root, &mut |entry| {
+                            if entry.as_ref().x == expected_x {
+                                x_entry = Some(entry);
+                            }
+                        })
+                    }
+                    let mut x_entry = x_entry.unwrap();
+                    root = unsafe {
+                        unlink::<Entry, HOOK_OFFSET>(root, x_entry.as_mut().hook.get_mut())
+                    };
+                }
+            }
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,3 @@
 pub mod linked_list;
 pub mod skew_heap;
+pub mod leftist_heap;
